1a6ff50c14548564243d12235e2dfce94fe929bf
shahramsamar <shahramsamar2010@gmail.com>
2024-11-02 14:59:19 +0330

    generate user  one or many

--- generate_user.py
+++ generate_user.py
@@ -1,35 +1,44 @@
 from database.database import SessionLocal
 from models.user import UserModel
+from faker import Faker
 
+fake = Faker()
 
 # create a new database session
 session = SessionLocal()
 
 try:
-    print("give me username and password for create user :")
-    username = input("Enter username :").lower()
-    password = input("Enter password :").lower()
+    # print("give me username and password for create user :")
+    # generate a list of sample students
+    # user = [UserModel(
+    #                         username=fake.user_name(),
+    #                         password=fake.password())
+    #                         for _ in range(5)]
+    # add all the sample  students ti the session in bulk
+    # session.bulk_save_objects(user)
     
-    # create a user
-    create_user = UserModel(username=username, password=password)
-    
-    # add the user to the session
-    session.add(create_user)
-    
-    # commit the transaction
-    session.commit()
-    print("Create User Added Successfully")
+    for _ in range(2):
+        print("Give me a username and password to create a user:")
+        username = input("Enter username: ").lower()
+        password = input("Enter password: ").lower()
 
-except Exception as e :
-    # Rollback the transaction in case of KeyError
+        # Create a user instance
+        user = UserModel(username=username, password=password)
+
+        # Add the user to the session
+        session.add(user)
+
+        # Commit the transaction
+        session.commit()
+        print("User created successfully.")
+
+except Exception as e:
+    # Rollback the transaction in case of an error
     session.rollback()
-    print("Error adding create user :{e}")
-
-
+    print(f"Error adding user: {e}")
 
 finally:
-    # close the session 
-    session.close()        
-    
+    # Close the session
+    session.close()
     
     
